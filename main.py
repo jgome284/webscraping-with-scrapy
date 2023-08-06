b0ce5fbf2810c7f378e8b9aaeb34af09d53e3a22
Jose Miguel Gomez <30394024+jgome284@users.noreply.github.com>
2023-08-05 20:45:10 -0400

    first pass at scraping with scrapy

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
+# Import a scrapy Selector
+from scrapy import Selector
+
+# Import requests
+import requests
+
+#store url of site for scraping content
+url = 'https://www.brookings.edu/articles/how-artificial-intelligence-is-transforming-the-world/'
+
+# Create the string html containing the HTML source
+html = requests.get( url ).content
+
+# Create the Selector object sel from html
+sel = Selector( text = html )
+
+# Print out the number of elements in the HTML document
+print( "There are {0} elements in the HTML document.").format(len(sel.xpath('//*')))
+
+#create a selector list of all paragraph elements
+psl = sel.xpath('//p') # select all paragraph elements
+
+#extract paragraph element text
+extracted = psl.extract()
+
+#loop through paragraph text
+for extract in extracted:
+    print(extract)
+
+#extract all hyperlinks
+links = sel.xpath('//a/@href')
 
