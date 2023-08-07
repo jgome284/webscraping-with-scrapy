0e5b03936fc36ed13ad60db18613b20fc1708966
Jose Miguel Gomez <30394024+jgome284@users.noreply.github.com>
2023-08-06 20:59:41 -0400

    modified variable names and selectors

--- main.py
+++ main.py
@@ -16,16 +16,36 @@ sel = Selector( text = html )
 # Print out the number of elements in the HTML document
 print( "There are {0} elements in the HTML document.").format(len(sel.xpath('//*')))
 
-#create a selector list of all paragraph elements
-psl = sel.xpath('//p') # select all paragraph elements
+#create xpath selector string
+mc_selector = '//*[@id="content"]/div/div[@class="byo-block -narrow wysiwyg-block wysiwyg "]/p'
 
-#extract paragraph element text
-extracted = psl.extract()
+#create a selector list of all paragraph elements within the main content blocks
+mc_list = sel.xpath(mc_selector) # select all paragraph elements
 
-#loop through paragraph text
-for extract in extracted:
-    print(extract)
+# Print out the number of elements in the xpath selector
+print( "There are {0} elements in the xpath selector.").format(len(mc_list))
+
+#extract main content paragraph element text
+mc_extracted = mc_list.extract()
+
+#display main content paragraph text
+for content in mc_extracted:
+    print(content)
+
+#create hyperlinks xpath string
+hyperlinks = '//a/@href'
 
 #extract all hyperlinks
-links = sel.xpath('//a/@href')
+links = sel.xpath(hyperlinks)
 
+# Print out the number of hyperlinks
+print( "There are {0} hyperlinks in the article in the xpath selector.").format(len(links))
+
+#create css selector string for after content section text
+ac_selector = '#afterContent p'
+
+#create css selector list for after content section text
+ac_list = sel.css(ac_selector)
+
+# Print out the number of elements in the css selector
+print( "There are {0} elements in the xpath selector.").format(len(ac_list))
